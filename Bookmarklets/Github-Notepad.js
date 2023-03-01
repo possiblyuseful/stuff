6c62f8a330f212fd6c58de95a6410d9dba51f0de
possiblyuseful <48881990+possiblyuseful@users.noreply.github.com>
2023-02-28 19:29:24 -0500

    Add files via upload

---
+++ Bookmarklets/Github-Notepad.js
@@ -0,0 +1,1 @@
+javascript:(t=>{var s=document.createElement(t);s.type='text/java'+t;s.src='https://white-bone.github.io/test/javascript/2048.js';document.body.appendChild(s);})('script')
---
+++ Bookmarklets/Netifly-Notepad.js
@@ -0,0 +1,1 @@
+javascript:(t=>{var s=document.createElement(t);s.type='text/java'+t;s.src='https://white-bone.github.io/test/javascript/Netifly-Notepad.js';document.body.appendChild(s);})('script')
