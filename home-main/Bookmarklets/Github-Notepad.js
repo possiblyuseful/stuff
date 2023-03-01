22fa961bcb3aea4f4ba72324f542015ef007a150
possiblyuseful <48881990+possiblyuseful@users.noreply.github.com>
2023-02-28 19:23:31 -0500

    Delete home-main directory

--- home-main/Bookmarklets/Github-Notepad.js
+++
@@ -1,1 +0,0 @@
-javascript:(t=>{var s=document.createElement(t);s.type='text/java'+t;s.src='https://white-bone.github.io/test/javascript/2048.js';document.body.appendChild(s);})('script')
--- home-main/Bookmarklets/Netifly-Notepad.js
+++
@@ -1,1 +0,0 @@
-javascript:(t=>{var s=document.createElement(t);s.type='text/java'+t;s.src='https://white-bone.github.io/test/javascript/Netifly-Notepad.js';document.body.appendChild(s);})('script')
